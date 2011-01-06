38ba9ce407e0f8188f813e273dfb8e8497530afd
nelsonab <nelsonab@0b82185e-49c8-4601-bd6e-747293a2f232>
2011-01-06 21:04:22 +0000

    Separated Zabcon files from the Zbxapi files.
    git-svn-id: https://svn.red-tux.net/trunk/ruby/zabcon@272 0b82185e-49c8-4601-bd6e-747293a2f232

---
+++ libs/zabcon_exceptions.rb
@@ -0,0 +1,45 @@
+#GPL 2.0  http://www.gnu.org/licenses/gpl-2.0.html
+#Zabbix CLI Tool and associated files
+#Copyright (C) 2009,2010 Andrew Nelson nelsonab(at)red-tux(dot)net
+#
+#This program is free software; you can redistribute it and/or
+#modify it under the terms of the GNU General Public License
+#as published by the Free Software Foundation; either version 2
+#of the License, or (at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+##########################################
+# Subversion information
+# $Id: zabcon_exceptions.rb 258 2010-12-28 22:49:21Z nelsonab $
+# $Revision: 258 $
+##########################################
+
+#------------------------------------------------------------------------------
+#
+# Class ZbxAP_ParameterError
+#
+# Exception class for parameter errors for Argument Processor calls.
+#
+#------------------------------------------------------------------------------
+
+require 'libs/zdebug'
+require 'libs/exceptions'
+
+#---------------------------------------------------------
+#These general exceptions are only used internally and are
+#not meant to be used by the end consumer of this program
+#---------------------------------------------------------
+
+class Redirect < Exception
+end
+
+class ZabconError < ZError
+end
