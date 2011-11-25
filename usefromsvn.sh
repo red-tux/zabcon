3e4f2b83d8362d5e3d14f4185c06b2adf2fc2ad4
richlv <richlv@0b82185e-49c8-4601-bd6e-747293a2f232>
2011-11-25 15:52:17 +0000

    - ugly script to quickly set up zabcon so that it can be run from the svn checkout
    git-svn-id: https://svn.red-tux.net/trunk/ruby/zabcon@353 0b82185e-49c8-4601-bd6e-747293a2f232

---
+++ usefromsvn.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# this script sets up zabcon so that it may be used directly from the svn checkout
+# if you want to start from an empty state (no existing checkout), uncomment the following 3 lines;
+# in that case, run this script from the directory in which "zabcon" and "api" directories reside
+
+#svn co -N http://svn.red-tux.net/trunk/ruby .
+#svn up api zabcon
+#cd zabcon
+
+ln -s ../api/zbxapi.rb zbxapi.rb
+cd libs
+ln -s ../api/libs/api_exceptions.rb api_exceptions.rb
+ln -s ../api/libs/exceptions.rb exceptions.rb
+ln -s ../api/libs/zdebug.rb zdebug.rb
