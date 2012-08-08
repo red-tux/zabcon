e643c9385555e45f903e27fa47bbd04499563cd1
nelsonab <nelsonab@0b82185e-49c8-4601-bd6e-747293a2f232>
2012-08-08 15:22:00 +0000

    Update to allow running Zabcon directly from svn checkouts.
    git-svn-id: https://svn.red-tux.net/trunk/ruby/zabcon@407 0b82185e-49c8-4601-bd6e-747293a2f232

--- usefromsvn.sh
+++ usefromsvn.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-# this script sets up zabcon so that it may be used directly from the svn checkout
-# if you want to start from an empty state (no existing checkout), uncomment the following 3 lines;
-# in that case, run this script from the directory in which "zabcon" and "api" directories reside
+# This script allows zabcon to be executed from SVN without the zbxapi
+# being previously installed.  This script assumes that it is being called
+# from within the zabcon directory inside the SVN repository.
 
-#svn co -N http://svn.red-tux.net/trunk/ruby .
-#svn up api zabcon
-#cd zabcon
+if [[ "x$RUBYLIB" == "z" ]]; then
+  export RUBYLIB="../api/"
+else
+  export RUBYLIB="../api/:$RUBYLIB"
+fi
 
-ln -s ../api/zbxapi.rb zbxapi.rb
-ln -s ../api/zbxapi zbxapi
-ln -s ../api/api_classes api_classes
-
-#No longer needed, zabcon uses a search path of "."
-#cd libs
-#ln -s ../api/libs/api_exceptions.rb api_exceptions.rb
-#ln -s ../api/libs/exceptions.rb exceptions.rb
-#ln -s ../api/libs/zdebug.rb zdebug.rb
-
-#cd -
+zabcon.rb $*
